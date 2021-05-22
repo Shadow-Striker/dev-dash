1858e665b7318074fbad98a76bb132641d9756ff
Shadow-Striker <82645925+Shadow-Striker@users.noreply.github.com>
2021-05-22 15:32:20 +0100

    Cars now spawn every 3 seconds and move backwards.

---
+++ Dev Dash Unity Project/Assets/Spawn.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour
+{
+    [SerializeField] private GameObject spawnObject;
+    [SerializeField] private float timeBtwnSpawns;
+    [SerializeField] private float timeLeftBtwnSpawns;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeLeftBtwnSpawns = timeBtwnSpawns;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SpawnCar();
+    }
+
+    void SpawnCar()
+    {
+        timeLeftBtwnSpawns -= Time.deltaTime;
+
+        if (timeLeftBtwnSpawns <= 0)
+        {
+            Instantiate(spawnObject, transform);
+            timeLeftBtwnSpawns = timeBtwnSpawns;
+        }
+    }
+}
