ac044265fba83acd31b6ac3cc4feda8c43b74f63
Shadow-Striker <82645925+Shadow-Striker@users.noreply.github.com>
2021-05-27 11:16:55 +0100

    Lane Switching and player movement is working now.

--- Dev Dash Unity Project/Assets/Scripts/PlayerController.cs
+++ Dev Dash Unity Project/Assets/Scripts/PlayerController.cs
@@ -10,10 +10,11 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private int currentLaneNumber = 1;
     [SerializeField] private bool tap, swipeLeft, swipeRight;
     private bool isDragging = false;
-    private bool moveLeft = false;
-    private bool moveRight = false;
+    [SerializeField] private bool moveLeft = false;
+    [SerializeField] private bool moveRight = false;
     private float distanceBtwnLanes = 2f;
     [SerializeField] private Vector2 startMousePos, endMousePos;
+    private Vector2 startingPos;
     [SerializeField] private Vector2[] lanePositions;
     public Vector3 mousePos;
 
@@ -31,6 +32,7 @@ public class PlayerController : MonoBehaviour
         swipeLeft = swipeRight = false;
         MouseCode();
         SwitchLanes();
+        LaneMovement();
     }
 
     private void MouseCode()
@@ -43,74 +45,68 @@ public class PlayerController : MonoBehaviour
 
         if(Input.GetMouseButtonUp(0))
         {
-            //float moveLength = (endMousePos - startMousePos).sqrMagnitude;
-
-           // if (moveLength < 30) return;
-
             endMousePos = Input.mousePosition;
 
             Vector2 moveDirection = (endMousePos - startMousePos).normalized;
-            print("Move Dir: " + moveDirection);
 
 
             if(moveDirection.x < 0)
             {
                 swipeLeft = true;
+                print("Swiped to the left");
             }
             else if(moveDirection.x > 0)
             {
                 swipeRight = true;
+                print("Swiped to the right");
             }
         }
-
-
     }
 
     private void SwitchLanes()
     {
+        if (moveLeft || moveRight) return;
+
         if (swipeLeft)
         {
-            if (transform.position.x != lanePositions[0].x)
+            if (currentLaneNumber != 0)
             {
+                startingPos = transform.position;
                 moveLeft = true;
-               // transform.position = new Vector3(lanePositions[currentLaneNumber - 1].x, transform.position.y, 0);
-                currentLaneNumber -= 1;
             }
         }
 
         else if (swipeRight)
         {
-            if (transform.position.x != lanePositions[2].x)
+            if (currentLaneNumber != 2)
             {
-                transform.position = new Vector3(lanePositions[currentLaneNumber + 1].x, transform.position.y, 0);
+                startingPos = transform.position;
+                moveRight = true;
+            }
+        }
+    }
+
+    private void LaneMovement()
+    {
+        if (moveLeft)
+        {
+            transform.position += Vector3.left * moveSpeed * Time.deltaTime;
+            if (transform.position.x <= lanePositions[currentLaneNumber - 1].x)
+            {
+                transform.position = new Vector2(lanePositions[currentLaneNumber - 1].x, transform.position.y);
+                currentLaneNumber -= 1;
+                moveLeft = false;
+            }
+        }
+
+        if (moveRight)
+        {
+            transform.position += Vector3.right * moveSpeed * Time.deltaTime;
+            if (transform.position.x >= lanePositions[currentLaneNumber + 1].x)
+            {
+                transform.position = new Vector2(lanePositions[currentLaneNumber + 1].x,transform.position.y);
                 currentLaneNumber += 1;
-            }
-        }
-
-        if(moveLeft && transform.position.x != -distanceBtwnLanes)
-        {
-            transform.position += Vector3.left * distanceBtwnLanes * Time.deltaTime;
-        }
-        else
-        {
-
-        }
-
-        while(moveLeft)
-        {
-            transform.position += Vector3.left * distanceBtwnLanes * Time.deltaTime;
-            if(transform.position.x <= -distanceBtwnLanes)
-            {
-                moveLeft = false;
-            }
-        }
-
-        while (moveRight)
-        {
-            transform.position += Vector3.right * distanceBtwnLanes * Time.deltaTime;
-            if (transform.position.x >= distanceBtwnLanes)
-            {
-                moveLeft = false;
+                moveRight = false;
             }
         }
     }
